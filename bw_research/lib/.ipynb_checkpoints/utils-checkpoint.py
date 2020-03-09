95a680c93f5570d8fa5c609804df7fbb8c5b56e2
vangelis <axokid318@gmail.com>
2020-03-09 15:11:08 +0000

    delayed commit from laptop

--- bw_research/lib/.ipynb_checkpoints/utils-checkpoint.py
+++ bw_research/lib/.ipynb_checkpoints/utils-checkpoint.py
@@ -8,6 +8,11 @@ bandwidth_default = 'bw'
 
 
 '''
-HELPERS
+|--- HELPERS ---|
 '''
 
+'''
+Returns the N relays which best represent the input dataframe
+'''
+def chooseNRelays(df, name_column=fingerprint_default, bandwidth_column=bandwidth_default):
+    pass
