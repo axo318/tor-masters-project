9104df360c7706e58d5c1a9540ac8d82ed5aaf03
vangeli <axokid318@gmail.com>
2020-04-10 19:25:29 +0100

    Done figure 4.2

--- bw_research/lib/.ipynb_checkpoints/my_time-checkpoint.py
+++ bw_research/lib/.ipynb_checkpoints/my_time-checkpoint.py
@@ -4,6 +4,7 @@ class Time:
     def __init__(self, string):
         self.string = string
         self.time = datetime.strptime(string, '%Y-%m-%d %H:%M:%S')
+        self.t = self.getT(string)
         self.seconds = self.time.timestamp()
         
     def toString(self):
@@ -11,3 +12,6 @@ class Time:
     
     def toSeconds(self):
         return self.seconds
+    
+    def getT(self, s):
+        return datetime.strptime(s.split(' ')[1], '%H:%M:%S')
